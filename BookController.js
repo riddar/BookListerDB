c91f6307ddd75fec315382bfb464346586d7077f
Unknown <fredrik.ridderheim@hotmail.com>
2018-03-13 13:46:19 +0100

    Add working TestFetch()

--- BookController.js
+++ BookController.js
@@ -1,38 +1,48 @@
-let Endpoint = "https://www.forverkliga.se/JavaScript/api/crud.php?requestKey";
-var bookArray = new Array();
+window.addEventListener('load', function(event){
 
-function GetAllBooks(event) {
+  let Endpoint = "https://www.forverkliga.se/JavaScript/api/crud.php?requestKey";
+
+  var div = document.getElementById("list")
+
+  function GetAllBooks(event) {
+      fetch(Endpoint)
+          .then(response => {
+              console.log('Response: ', response)
+              return response.text()
+          })
+          .then(text => {
+              console.log('text: ', text)
+          })
+  }
+
+  function CreateBook(event) {
     fetch(Endpoint)
-        .then(response => {
-            console.log('Response: ', response)
-            return response.text()
-        })
-        .then(text => {
-            console.log('text: ', text)
-        })
-}
+      .then(response => {
 
-function CreateBook(event) {
-	fetch(Endpoint)
-		.then(response => {
+      })
+  }
 
-		})
-}
+  function TestFetch(event){
+    console.log("test")
+    fetch(Endpoint)
+      .then(function(response) {
+        console.log('Response: ', response)
+        return response.json()
+      })
+      .then(function(response){
+        console.log('Response: ', response)
+        return response.text();
+      })
+      .then(function(text) { 
+        console.log('text: ', text)
+        return text.text()
+      })
+      .catch(function(message){
+        console.log('Error: ', message)
+        return text.text()
+      })   
+  }
 
-function TestFetch(event){
-	fetch(Endpoint)
-  	.then(function(response) {
-    	return response.json()
-    })
-  	.then(function(text) { 
-  		return text.json()
-  	})
-  	.catch(function(message){ 
-  		return message.json()
-  	})
-}
-
-
-//ajaxBtn.addEventListener('click', TestFetch)
-var div = document.getElementById("list");
-div.innerText += 'test';
+  var ajaxBtn = document.getElementById('CreateBtn').addEventListener('click', TestFetch)
+  div.innerText = ajaxBtn;
+})
