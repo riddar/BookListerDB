8bd264dac9e8089225bab16b4fe56efb34c44aea
Unknown <fredrik.ridderheim@hotmail.com>
2018-03-13 12:35:35 +0100

    added code Behind

--- Controllers/BookController.js
+++ Controllers/BookController.js
@@ -1,1 +1,12 @@
-// JavaScript source code
+let Endpoint = "https://www.forverkliga.se/JavaScript/api/crud.php?requestKey";
+
+function GetAllBooks(event) {
+    fetch(Endpoint)
+        .then(response => {
+            console.log('Response: ', response);
+            return response.text()
+        })
+        .then(text => {
+            console.log('text: ', text);
+        })
+}
