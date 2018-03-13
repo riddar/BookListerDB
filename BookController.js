f426c093b3a4011540f7c31be231aabeec8ee011
Unknown <fredrik.ridderheim@hotmail.com>
2018-03-13 15:52:02 +0100

    made Changes

--- BookController.js
+++ BookController.js
@@ -1,11 +1,17 @@
 window.addEventListener('load', function(event){
 
-  let Endpoint = "https://www.forverkliga.se/JavaScript/api/crud.php?requestKey";
+  let Endpoint = "https://www.forverkliga.se/JavaScript/api/crud.php?";
 
   var div = document.getElementById("list")
 
-  function CreateBook(event) {
-    fetch(Endpoint, )
+  function CreateBooks(event) {
+
+  }
+
+  function GetAllBooks(event) {
+    var key = ("key: ", FetchKey);
+    console.log(FetchKey)
+    fetch(Endpoint + "op=select&" + FetchKey)
       .then(function(response){
         console.log('Response: ', response.status)
         if(response.status == 200){
@@ -23,30 +29,42 @@ window.addEventListener('load', function(event){
       })  
   }
 
-  function TestFetch(event){
-    fetch(Endpoint)
+  function UpdateBook(event){
+
+  }
+
+  function DeleteBook(event){
+
+  }
+
+  function FetchKey(event){
+    fetch(Endpoint + "requestKey")
       .then(function(response){
-        console.log('Response: ', response.status)
         if(response.status == 200){
-          return response.text();
+          return response.json();
         }
         return;
       })
       .then(function(text) { 
-        console.log('text: ', text)
-        div.innerHTML = text;
-        return text;
+        if(text.status != "error"){
+          console.log('text: ', text.key)
+          div.innerHTML = text;
+          return text;
+        }
+        else{
+          console.log()
+        }
+        
       })
       .catch(function(message){
         console.log('Error: ', message)
-      })  
+      })
   }
 
-  var query = document.getElementById('CreateBtn').addEventListener('click', TestFetch)
-  if(query != null){
-    div.innerHTML += query;
-  }
   
-  
+  document.getElementById('CreateBtn').addEventListener('click', GetAllBooks)
+  document.getElementById('ReadBtn').addEventListener('click', FetchKey)
+  //document.getElementByClass('UpdateBtn').addEventListener('click', UpdateBook)
+  //document.getElementByClass('DeleteBtn').addEventListener('click', DeleteBook)
 
 })
